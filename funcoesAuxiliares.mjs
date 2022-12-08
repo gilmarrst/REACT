d5b2a6225195756eef14b6187ed82c702be263d1
Gilmar Santos <gilmar.santos@bancointer.com.br>
2022-12-08 18:15:32 -0300

    aula  de importação de funções

---
+++ funcoesAuxiliares.mjs
@@ -0,0 +1,11 @@
+export default {
+
+    gets: function gets()
+    {
+        return 10;
+    },
+    print: function print(texto)
+    {
+        console.log(texto);
+    }
+};
---
+++ main.mjs
@@ -0,0 +1,5 @@
+import a from "./funcoesAuxiliares.mjs";
+
+(function(){
+    console.log(a.gets());
+})()
